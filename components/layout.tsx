e507296dd8ec37dfbfcc3ccaece74c037e486396
SiraKim0 <dosira686@naver.com>
2023-03-16 22:03:17 +0900

    type, css 추가

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,8 @@
+import React, { PropsWithChildren } from "react";
 import Header from "./header";
 import Footer from "./footer";
 
-export default function Layout({ children }) {
+export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className="bg-primary">
       <Header />
